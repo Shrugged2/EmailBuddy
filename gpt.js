afaf1ba4600b45e1209e42362a9e93ade51fbfa7
Rick <42774042+Shrugged2@users.noreply.github.com>
2023-01-06 23:39:03 -0600

    Update gpt.js

--- gpt.js
+++ gpt.js
@@ -1,16 +1,24 @@
-fetch('https://api.openai.com/v1/text-davinci/generations', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer YOUR_API_KEY'
-  },
-  body: JSON.stringify({
-    "prompt": "What is the capital of France?",
-    "model": "text-davinci-002",
-    "max_tokens": 100
-  })
-})
-.then(response => response.json())
-.then(data => {
-  console.log(data.choices[0].text);
+chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+  const tab = tabs[0];
+  chrome.tabs.sendMessage(tab.id, { action: 'getText' }, response => {
+    const prompt = response.text;
+
+    
+    fetch('https://api.openai.com/v1/text-davinci/generations', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer YOUR_API_KEY'
+      },
+      body: JSON.stringify({
+        "prompt": prompt,
+        "model": "text-davinci-002",
+        "max_tokens": 100
+      })
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data.choices[0].text);
+    });
+  });
 });
